5627e862ef5034469da45cfb2ff8935fc5b52a40
engmg2011 <eng.mg2011@gmail.com>
2016-08-22 17:16:46 +0200

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,11 @@
-function printData(){
-	
-	//FEED_URL = 'http://www.mgovmagazine.com/component/k2/itemlist?format=feed&amp;type=rss' ;
+function printData(feeds_url = ''){ 
+	var api ; 
+	if(feeds_url == '')
+		api ="http://mgovmagazine.com/export2.php"; 
+	else
+		api = "http://mgovmagazine.com/export2.php?rss_mod_id="+feeds_url ;
 
-	api ="http://mgovmagazine.com/export2.php"; 
+	//console.log(api );
 	//api ="http://localhost/mgov/export2.php"; 
 	$('#My_data').html('<img src="icon.png" class="waiting-logo imageSpin" />');
 	
@@ -17,17 +20,18 @@ function printData(){
 			titlefree =  title.replace(/"/g, "");; 
 			//console.log(titlefree);
 			//console.log(item.link[0]);
+			//console.log(item);
 			$('#My_data').append('<div class="content_item">\
 				<div class="img">\
 					<a  onclick="openNews(\''+item.link[0]+'\' ,\''+titlefree+'\' )" data-role="button" data-rel="dialog" data-transition="pop">\
 						<img src="'+item.image[0]+'" />\
 					</a>\
 				</div>\
-				<a onclick="openNews(\''+item.link[0]+'\' ,\'sssssssss\' )" data-role="button" data-rel="dialog" data-transition="pop">\
+				<a onclick="openNews(\''+item.link[0]+'\' ,\''+titlefree+'\' )" data-role="button" data-rel="dialog" data-transition="pop">\
 						<h3 class="title">'+titlefree+'</h3>\
 				</a>\
 				<!--h4 class="description">'+titlefree+'</h4-->\
-				<h6 class="time">'+item.pubDate[0]+'</h6>\
+				<h6 class="time">'+item.pubDate+'</h6>\
 			</div> ');
 		}); 
 	});  
@@ -60,13 +64,31 @@ function printPDFData(){
 }
 
 function openNews(link , title){
-	console.log(link);
-	console.log(title);
+	// console.log(link);
+	// console.log(title);
 	$('#popup_button').trigger('click'); 
 	$('#newsFrame').html('<iframe id="newsShow" src="'+link+'" /></iframe>');
 	$('#newsTitle').html(title);
 }
+ 
 
+function getFeeds(feeds_url_id = '' ){  
+	$('#megamenu>.header').css('background','#eee url("res/images/rss/'+feeds_url_id+'.jpg") 0px no-repeat');  
+	$('#one>.ui-header').css('background','#eee url("res/images/rss/'+feeds_url_id+'.jpg") 0px no-repeat'); 
+	$('#one>.ui-header').css('background-size','100%'); 
+	printData(feeds_url_id);  
+	$("#megamenu").animate({width:'toggle'},350); 
+}
+ 
+$('.showHome').click(function(e){   
+	$("#megamenu").animate({width:'toggle'},350);
+})
+$('.sideMenu>li').click(function(){ 
+	catname = $(this).children('a').html();
+	$('.info').html(catname);
+	$('.sideMenu>li').removeClass('active');
+	$(this).addClass('active');
+})
 
 $(function(){
 	printData();
